08f6b4fb39d1ef52697b03235cdcb8b74c986efc
acr1991 <acr1991@live.nl>
2020-02-26 17:18:13 +0100

    Initalized db connection

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// importing dependencies
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
 //DO NOT CODE UNDER THIS LINE
+// make the app listen on a the port we specified
 app.listen(port, () => console.log("Listening on port ", port));
+
+app.get("/user", (req, res) => {
+  res.send("hello");
+});
